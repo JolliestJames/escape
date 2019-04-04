86cd61655f3564ec912fc05b89c20f399c04afa0
JolliestJames <cloudismydog@gmail.com>
2019-04-03 21:03:17 -0700

    Escape with squares

--- escape.py
+++ escape.py
@@ -10,3 +10,31 @@ def draw_line():
     while not escaped(t.position()):
         t.left(angle)
         t.forward(step)
+
+def draw_square(t, size):
+    L = []
+    for i in range(4):
+        t.forward(size)
+        t.left(90)
+        store_position_data(L, t)
+        return L
+
+def store_position_data(L, t):
+    position = t.position()
+    L.append([position[0], position[1], escaped(position)])
+
+def draw_squares(number):
+    t = turtle.Turtle()
+    L = []
+    for i in range(1, number + 1):
+        t.penup()
+        t.goto(-i, -i)
+        t.pendown()
+        L.extend(draw_square(t, i * 2))
+        return L
+
+def draw_squares_until_escaped(n):
+    t = turtle.Turtle()
+    L = draw_squares(n)
+    with open("data_square", "wb") as f:
+        pickle.dump(L, f)
