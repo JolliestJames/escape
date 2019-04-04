8a7ef25e4a099c5aa07c7ae5d310be425d652a9c
JolliestJames <cloudismydog@gmail.com>
2019-04-03 23:59:06 -0700

    Add main block

--- escape.py
+++ escape.py
@@ -1,3 +1,22 @@
+import argparse
+import inspect
+import random
+import pickle
+import turtle
+
+def draw_bag():
+    turtle.shape('turtle')
+    turtle.pen(pencolor='green', pensize=5)
+    turtle.penup()
+    turtle.goto(-35, 35)
+    turtle.pendown()
+    turtle.right(90)
+    turtle.forward(70)
+    turtle.left(90)
+    turtle.forward(70)
+    turtle.left(90)
+    turtle.forward(70)
+
 def escaped(position):
     x = int(position[0])
     y = int(position[1])
@@ -7,6 +26,8 @@ def draw_line():
     angle = 0
     step = 5
     t = turtle.Turtle()
+    t.shape('turtle')
+    t.pen(pencolor='green', pensize=5)
     while not escaped(t.position()):
         t.left(angle)
         t.forward(step)
@@ -17,7 +38,7 @@ def draw_square(t, size):
         t.forward(size)
         t.left(90)
         store_position_data(L, t)
-        return L
+    return L
 
 def store_position_data(L, t):
     position = t.position()
@@ -25,28 +46,32 @@ def store_position_data(L, t):
 
 def draw_squares(number):
     t = turtle.Turtle()
+    t.shape('turtle')
     L = []
     for i in range(1, number + 1):
         t.penup()
         t.goto(-i, -i)
         t.pendown()
         L.extend(draw_square(t, i * 2))
-        return L
+    return L
 
 def draw_squares_until_escaped(number):
     t = turtle.Turtle()
+    t.shape('turtle')
     L = draw_squares(number)
     with open("data_square", "wb") as f:
         pickle.dump(L, f)
 
 def draw_triangles(number):
     t = turtle.Turtle()
+    t.shape('turtle')
     for i in range(1, number):
         t.forward(i*10)
         t.right(120)
 
 def draw_spirals_until_escaped():
     t = turtle.Turtle()
+    t.shape('turtle')
     t.penup()
     t.left(random.randint(0, 360))
     t.pendown()
@@ -70,3 +95,29 @@ def draw_random_spirangles():
 
     with open("data_rand", "wb") as f:
         pickle.dump(L, f)
+
+if __name__ == '__main__':
+    fns = {
+        "line": draw_line,
+        "squares": draw_squares_until_escaped,
+        "triangles": draw_triangles,
+        "spirangles": draw_random_spirangles
+    }
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--function", choices=fns, help="One of" + ','.join(fns.keys()))
+    parser.add_argument("-n", "--number", default=50, type=int, help="How many?")
+    args = parser.parse_args()
+
+    try:
+        f = fns[args.function]
+        turtle.setworldcoordinates(-70., -70., 70., 70.)
+        draw_bag()
+        turtle.hideturtle()
+        if len(inspect.getargspec(f).args) == 1:
+            f(args.number)
+        else:
+            f()
+        turtle.mainloop()
+    except KeyError:
+        parser.print_help()
